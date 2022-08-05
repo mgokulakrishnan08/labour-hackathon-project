102061b5fbf90e85d833ba32a53653bd3515f4dd
mgokulakrishnan31 <92560183+mgokulakrishnan31@users.noreply.github.com>
2022-08-05 13:11:30 +0530

    coded login.html

--- ministry_of_labour_and_welfare/DigiResume/views.py
+++ ministry_of_labour_and_welfare/DigiResume/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
+from .models import *
 
 # Create your views here.
 
 def index(request):
+    if request.GET:
+        id=request.GET.dict()['id'].upper()
+        return redirect('/view_details/')
     return render(request,'DigiResume/index.html')
 
 def login(request):
-    return render(request,'DigiResume/login.html')
+    flag=True
+    if request.GET:
+        id=request.GET.dict()['id'].upper()
+        password=request.GET.dict()['password']
+        global sector
+        if 'EDU' in id:
+            sector=1
+            x=Institution.objects.get(inst_code=id).password
+        elif 'ORG' in id:
+            sector=2
+            x=Institution.objects.get(org_code=id).password
+        elif 'SEV' in id:
+            sector=3
+            x=Institution.objects.get(sev_code=id).password
+        if password==x:
+            return redirect('/home/')
+        else:
+            flag=False
+    return render(request,'DigiResume/login.html',{'flag':flag})
 
 def home(request):
-    return render(request,'DigiResume/home.html')
+    return render(request,'DigiResume/home.html',{'sector':sector})
 
 def register(request):
     return render(request,'DigiResume/register.html')
